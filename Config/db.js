a4a10130ad0ae4e9bfc329c7674a5354ceaedb7e
Arishya Afreen <arishya2007atm@gmail.com>
2026-03-06 22:12:02 +0530

    LibSpace Backend API

---
+++ Config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+  try {
+
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("Connected to MongoDB");
+
+  } catch (error) {
+
+    console.log("Database connection failed:", error);
+
+  }
+};
+
+export default connectDb;
