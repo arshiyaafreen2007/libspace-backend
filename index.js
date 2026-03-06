c07da182c3deefb7b5fdee7d4314466b16ab4ed3
Arishya Afreen <arishya2007atm@gmail.com>
2026-03-07 01:48:44 +0530

    Added root route for deployment check

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDb from "./Config/db.js";
 import userRouter from "./routes/user.route.js";
 import bookRouter from "./routes/book.route.js";
+
 dotenv.config();
 
 const app = express();
@@ -11,10 +12,17 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
+/* Root Route (for checking server) */
+app.get("/", (req, res) => {
+  res.send("LibSpace Backend API is running successfully 🚀");
+});
+
+/* Routes */
+app.use("/api/authuser", userRouter);
+app.use("/api/book", bookRouter);
+
+/* Start Server */
 app.listen(PORT, () => {
   connectDb();
   console.log(`Our server is working at PORT :${PORT}`);
 });
-
-app.use("/api/authuser", userRouter);
-app.use("/api/book", bookRouter);
